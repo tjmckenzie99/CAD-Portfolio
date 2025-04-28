c1057f68a2bf4925b29031eb240ea0e9dd4b0a91
tjmckenzie99 <162842155+tjmckenzie99@users.noreply.github.com>
2025-04-28 16:23:13 -0400

    Update

--- Transmission Loss Box/Mesh Files/command1.tcl
+++ Transmission Loss Box/Mesh Files/command1.tcl
@@ -135,3 +135,59 @@ return; # Stop script and return to application
 return; # Stop script and return to application 
 # *quit 1; # Uncomment to exit application
 *do_markrejectclear 0
+*begin "version 2025.0.0.24  4-25-2025  18:38:38"
+*createstringarray 20 " 0 penalty value              0.00    0.00    0.80    1.00   10.00" \
+  "  1 min length        1 1.0   1.000   0.900   0.400   0.200   0.100    1   59    0" \
+  "  2 max length        1 1.0   1.000   1.200   1.500   2.000   3.000    0   39    1" \
+  "  3 aspect ratio      1 1.0   1.000   2.000   4.400   5.000  10.000    0   41    2" \
+  "  4 warpage           1 1.0   0.000   5.000  13.000  15.000  30.000    0   56    3" \
+  "  5 max angle quad    1 1.0  90.000 110.000 134.000 140.000 160.000    0   28    4" \
+  "  6 min angle quad    1 1.0  90.000  70.000  46.000  40.000  20.000    0   61    5" \
+  "  7 max angle tria    1 1.0  60.000  80.000 112.000 120.000 150.000    0   19    6" \
+  "  8 min angle tria    1 1.0  60.000  50.000  34.000  30.000  15.000    0   22    7" \
+  "  9 skew              1 1.0   0.000  10.000  34.000  40.000  70.000    0   46    8" \
+  " 10 jacobian          1 1.0   1.000   0.900   0.700   0.600   0.300    0   57    9" \
+  " 11 chordal dev       0 1.0   0.000   0.300   0.800   1.000   2.000    0   29   10" \
+  " 12 taper             1 1.0   0.000   0.200   0.500   0.600   0.900    0   53   11" \
+  " 13 % of trias        1 1.0   0.000   6.000  10.000  15.000  20.000    0    0   -1" \
+  " 14 QI color legend            32      32       7       6       3           3   -1" \
+  " 15 time_step         0         1.000                       0.01            0   59   12" \
+  " 16 area_skew         0         1.000                      0.500            0   39   13" \
+  " 17 cell_squish       0         1.000                      0.600            0   53   14" \
+  " 18 equi_skew         0         1.000                      0.600            0   34   15" \
+  "   Global_solver -1"
+*setqualitycriteria 1 20 0
+*templatefileset "C:/Program Files/Altair/2025/hwdesktop/templates/feoutput/optistruct/optistruct"
+*menufilterset "*"
+*menufilterdisable 
+*setoption show_scale=0
+*setscaleoption position=bottom
+*viewset 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 10 10
+*setoption display_color-5=2
+*setoption highlight-active_color=#FFFFFF
+*setoption highlight-glow_color=#FFFFFF
+*graphicfont 2
+*menufont 2
+*ME_CoreBehaviorAdjust "allowable_actions_policy=default"
+*loaddefaultattributevaluesfromxml 
+*setoption highlight-active_color=#FFFFFF
+*setoption highlight-glow_color=#FFFFFF
+*hwct_openlibrary modules usersettingsfilepath=C:/Users/tjmck/.altair/AltairPartLibrarySettings2020.xml  workspacename=workspace  user=tjmck  libraryname=Part_Library  librarytype=LOCAL
+*clearmarkall 3
+*clearmarkall 1
+*setoption topoedgecolor=0
+*setoption topofacecolor=4
+*readfile "C:\\Users\\tjmck\\OneDrive\\Documents\\GitHub\\CAD-Portfolio\\Transmission Loss Box\\Mesh Files\\Transmission Loss Box Mesh 2.hm"  0
+*setoption topofacecolor=4
+*rotateabout 1 13.5123377 -3.26893759 10.75
+*viewset -0.770391492 0.442212678 0.45928738 0 0.546372733 0.829170986 0.118119909 0 -0.328593649 0.341940674 -0.880399222 0 10.7451027 -0.672327892 -18.9118977 1 -44.8726401 -23.65 41.4673251 23.65
+*rotateabout 1 -10.5 12.875 -1.9545455
+*viewset -0.756062649 0.619275659 -0.211818152 0 0.65308819 0.735070791 -0.182062482 0 0.0429544729 -0.275986576 -0.960201189 0 10.2478018 1.19060649 -22.2496341 1 -44.8726401 -23.65 41.4673251 23.65
+*rotateabout 1 5.99701118 -10.6802235 -10.75
+*viewset -0.984841053 0.153710787 0.080380928 0 0.14092563 0.979212943 -0.145883441 0 -0.101133904 -0.132344269 -0.986030896 0 4.6008278 8.13425178 -23.8932244 1 -44.8726401 -23.65 41.4673251 23.65
+*rotateabout 1 5.99701309 -10.6802197 -10.75
+*viewset -0.9826178 -0.0736258766 0.170415635 0 -0.00682429939 0.931694867 0.363177786 0 -0.185514655 0.35570199 -0.916002405 0 2.10240014 14.2365865 -18.2434717 1 -44.8726401 -23.65 41.4673251 23.65
+# Session ended at "4-25-2025  18:42:45"
+return; # Stop script and return to application 
+# *quit 1; # Uncomment to exit application
+*do_markrejectclear 0
